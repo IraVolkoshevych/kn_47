d5a03c2946d387524cd989b2201c7afce21712af
IvanovVadym <vadym.ivanov@ukr.net>
2018-09-23 22:41:07 +0300

    change procGetForms_NotStar.sql

--- Організація сховищ та просторів даних/Lab2/backend/HoReDLabs/Program.cs
+++ Організація сховищ та просторів даних/Lab2/backend/HoReDLabs/Program.cs
@@ -9,23 +9,42 @@ namespace HoReDLabs
         public ILab2Service _labService;
         static void Main(string[] args)
         {
+            int labNumber;
+            Console.WriteLine("Input number of lab");
+            labNumber = Console.Read();
             var dbContext = new DbContext();
-            var lab2NotStar = new Lab2Sevice(dbContext);
             Stopwatch sw = new Stopwatch();
-            sw.Start();
-            lab2NotStar.GetFormInfo();
-            sw.Stop();
-            Console.WriteLine("star = {0}", sw.Elapsed);
+            switch (labNumber)
+            {
+                case 2:
+                        var lab2NotStar = new Lab2Sevice(dbContext);
+                        sw.Start();
+                        lab2NotStar.GetFormInfo();
+                        sw.Stop();
+                        Console.WriteLine("star = {0}", sw.Elapsed);
 
-            dbContext.SetConnectionString("MyConStringSTORE_NOTSTAR");
-            sw.Reset();
+                        dbContext.SetConnectionString("MyConStringSTORE_NOTSTAR");
+                        sw.Reset();
 
-            var lab2Star = new Lab2Sevice(dbContext);
-            sw.Start();
-            lab2Star.GetFormInfo();
-            sw.Stop();
-            Console.WriteLine("snowflake = {0}", sw.Elapsed);
-            Console.ReadKey();
+                        var lab2Star = new Lab2Sevice(dbContext);
+                        sw.Start();
+                        lab2Star.GetFormInfo();
+                        sw.Stop();
+                        Console.WriteLine("snowflake = {0}", sw.Elapsed);
+                        Console.ReadKey();
+                    break;
+                case 3:
+                    dbContext.SetConnectionString("MyConString");
+                    var lab3NotStar = new Lab2Sevice(dbContext);
+                    sw.Reset();
+
+                    sw.Start();
+                    lab3NotStar.GetFormInfo();
+                    sw.Stop();
+                    Console.WriteLine("star = {0}", sw.Elapsed);
+                    break;
+            };
+            
         }
     }
 }
