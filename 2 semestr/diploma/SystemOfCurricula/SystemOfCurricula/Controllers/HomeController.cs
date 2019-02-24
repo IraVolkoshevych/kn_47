cc2a1035d60b29b34b3b672dfa68f5fba1a45a60
IvanovVadym <vadym.ivanov@ukr.net>
2019-02-24 21:52:16 +0200

    added controller for getting data for specialty map

--- 2 semestr/diploma/SystemOfCurricula/SystemOfCurricula/Controllers/HomeController.cs
+++ 2 semestr/diploma/SystemOfCurricula/SystemOfCurricula/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using SystemOfCurricula.Models;
+
 namespace SystemOfCurricula.Controllers
 {
     public class HomeController : Controller
@@ -12,24 +12,6 @@ namespace SystemOfCurricula.Controllers
         {
             ViewBag.Title = "Home Page";
 
-            using (var db = new SystemOfCurriculaContext())
-            {
-                var teachers = db.Set<Teacher>();
-                teachers.Add(new Teacher {
-                    TeacherFirstName = "Ihor",
-                    TeacherLastName = "Kohut"
-                });
-
-                db.SaveChanges();
-
-                var teschersForRemoving = teachers.Where(
-                    teacher => teacher.TeacherFirstName == "Ihor"
-                    && teacher.TeacherLastName == "Kohut");
-                teachers.RemoveRange(teschersForRemoving);
-
-                db.SaveChanges();
-            }
-            
             return View();
         }
     }
