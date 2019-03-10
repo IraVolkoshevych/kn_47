bde323a201964eda36c76fc2109cefc6bed8b2c0
o.shandra <oleghsandra@gmail.com>
2019-03-10 22:43:46 +0200

    SHandra the best

---
+++ 2 семестр/Розподілені системи/TRSPO_Labs/Lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Варiант 25
+            //Кiлькiть процесiв 1
+            Console.WriteLine("Процеси 1");
+            DoJob(11);
+
+            Thread.Sleep(0);
+
+            //Кiлькiть процесiв 2
+            Console.WriteLine("\nПроцеси 2");
+            DoJob(15);
+        }
+
+        static void DoJob(int count)
+        {
+            for (var i = 1; i <= count; i++)
+            {
+                var proccess = new Thread((index) =>
+                {
+                    Console.WriteLine("Proccess " + (int)index);
+                });
+
+                proccess.Start(i);
+            }
+        }
+    }
+}
